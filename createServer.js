f67f63b536ca4c4cf69512ef73f867efe4833894
David Liu <rumalzliu@gmail.com>
2016-07-22 23:20:00 -0400

    finish program

--- createServer.js
+++ createServer.js
@@ -8,4 +8,8 @@ http.createServer(function (request, response)) {
 	response.writeHead(200, {'Content-Type': "text/plain'});
 
 	//Send response as "Hello World"
+	response.end('Hello World\n');
+}).listen(8081);
 
+//Console prints message
+console.log('Server running at http://127.0.0.1:8081/');
