1f2d1b5fa2d1d00f82a0a02e6b44112b9231da3c
David Liu <rumalzliu@gmail.com>
2016-07-23 23:06:32 -0400

    fixed syntax errors

--- createServer.js
+++ createServer.js
@@ -1,13 +1,13 @@
 var http = require("http");
 
-http.createServer(function (request, response)) {
+http.createServer(function (request, response) {
 
 	// Send HTTP header
 	// HTTP status: 200: Ok
 	// Content Type: text/plain
-	response.writeHead(200, {'Content-Type': "text/plain'});
+	response.writeHead(200, {'Content-Type': 'text/plain'});
 
-	//Send response as "Hello World"
+	// Send the response body as "Hello World"
 	response.end('Hello World\n');
 }).listen(8081);
 
